277fe0216d447b20408ad918f229ff2a0bca76e9
mattted <medward4@gmail.com>
2020-06-13 14:36:00 -0700

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
 
 class CreateTodo extends Component {
+  constructor() {
+    super()
+    this.state = {
+      text: '',
+    };
+  }
+
+  handleChange = e => {
+    this.setState({
+      text: e.target.value
+    });
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    this.props.addTodo(this.state)
+  }
+
   render() {
     return(
       <div>
-        Create Todo Component
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <label>add todo</label>
+            <input type='text' onChange={this.handleChange} value={this.state.text}/>
+          </p>
+          <input type='submit' />
+        </form>
+        {this.state.text}
       </div>
     )
   }
 }
 
-export default CreateTodo;
+export default connect(null, mapDispatchToProps)(CreateTodo);
+
+const mapDispatchToProps = dispatch => {
+  addTodo: (formData) => dispatch({ type: "ADD_TODO", payload: formData})
+};
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -3,6 +3,12 @@ export default function manageTodo(state = {
 }, action) {
 
   console.log("reducer received this action:", action);
+  switch (action) {
+    case 'ADD_TODO':
+      return { todos: state.todos.concat(action.payload.text) }
+    default:
+      return state
+  };
 
   return state;
 }
